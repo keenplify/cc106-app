0a829ae95d3645a98cdc308574444bea9d10960c
keenplify <keenplify>
2023-04-26 23:06:07 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cc106-app',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
